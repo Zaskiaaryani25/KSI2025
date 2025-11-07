eb546d0e5c104e3b617c6e3ac4145d526b97401d
Muhammad Agus Rizal <rzllas25@gmail.com>
2025-11-07 20:39:51 +0700

    Menambahkan file koneksi.php untuk koneksi database

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "db_latihan";
+$dbname = "db_ksi";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
